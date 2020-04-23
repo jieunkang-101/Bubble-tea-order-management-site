5aef8384b420472c1584d99205dd1df96660eaec
Jiwon <jiwon1.han@gmail.com>
2020-04-23 12:41:54 -0700

    Replaced diagram
    Co-authored-by: Jieun Kang <jieunkang101@gmail.com>

--- src/components/BubbleTeaControl.jsx
+++ src/components/BubbleTeaControl.jsx
@@ -19,7 +19,7 @@ class BubbleTeaControl extends React.Component {
             quantity: 5,
             img : "https://images.squarespace-cdn.com/content/v1/58d1f7308419c23d328cdb22/1559894022209-RV13I0JU6Y5N4P7XZKQU/ke17ZwdGBToddI8pDm48kMXRibDYMhUiookWqwUxEZ97gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z4YTzHvnKhyp6Da-NYroOW3ZGjoBKy3azqku80C789l0luUmcNM2NMBIHLdYyXL-Jww_XBra4mrrAHD6FMA3bNKOBm5vyMDUBjVQdcIrt03OQ/Brown+Sugar+Bubble+Tea",
             id: v4(),
-            message: ""
+            message: "Available"
           },
           {
             name: "Taro milk Tea",
@@ -27,7 +27,7 @@ class BubbleTeaControl extends React.Component {
             quantity: 10,
             img: "https://cdn.shopify.com/s/files/1/0189/8074/files/shutterstock_409592143_grande.jpg?v=1504742648",
             id: v4(),
-            message: ""
+            message: "Available"
           },
           {
             name: "Matcha milk Tea",
@@ -35,7 +35,7 @@ class BubbleTeaControl extends React.Component {
             quantity: 6,
             img : "https://hocphache.com.vn/wp-content/uploads/2017/10/hinh-tra-sua-thai-tra-xanh.jpg",
             id: v4(),
-            message: ""
+            message: "Available"
           }
       ],
       
@@ -71,15 +71,22 @@ class BubbleTeaControl extends React.Component {
   // }
 
   handleRestockTea = (bubbleTeaToRestock) => {
+    this.state.selectedBubbleTea.message = "Available";
     this.state.selectedBubbleTea.quantity += 10;
     const newMasterBubbleTeaList = this.state.masterBubbleTeaList.filter(bubbleTea => bubbleTea.id !== this.state.selectedBubbleTea.id).concat(bubbleTeaToRestock);
     this.setState({masterTicketList: newMasterBubbleTeaList});
   }
 
   handleSellTea = (bubbleTeaToSell) => {
-    this.state.selectedBubbleTea.quantity > 0 ? this.state.selectedBubbleTea.quantity -- : this.state.selectedBubbleTea.message += "out of stock"
+    this.state.selectedBubbleTea.quantity > 0 ? this.state.selectedBubbleTea.quantity -- : this.state.selectedBubbleTea.message = "out of stock"
     const newMasterBubbleTeaList = this.state.masterBubbleTeaList.filter(bubbleTea => bubbleTea.id !== this.state.selectedBubbleTea.id).concat(bubbleTeaToSell);
     this.setState({masterTicketList: newMasterBubbleTeaList});
+  }
+
+  handleDeleteTea = (id) => {
+    const newMasterBubbleTeaList = this.state.masterBubbleTeaList.filter(bubbleTea => bubbleTea.id !== id);
+    this.setState({masterBubbleTeaList: newMasterBubbleTeaList});
+    this.setState({selectedBubbleTea: null});
   }
 
   handleAddingNewBubbleTeaToList = (newBubbleTea) => {
@@ -97,13 +104,11 @@ class BubbleTeaControl extends React.Component {
   // }
 
 
-
   setVisibility  = () => {
     if (this.state.selectedBubbleTea != null) {
       return {
         buttonText: "Return to the Main Page",
-        // order: this.state.selectedBubbleTea.quantity > 0 ? <BubbleTeaDetail bubbleTea = {this.state.selectedBubbleTea}onSellingTea={this.handleSellTea}   onRestockingTea={this.handleRestockTea}/> : <BubbleTeaDetail bubbleTea = {this.state.selectedBubbleTea.message += 'out of stock'}  />
-        order:  <BubbleTeaDetail bubbleTea = {this.state.selectedBubbleTea} onSellingTea={this.handleSellTea}  onRestockingTea={this.handleRestockTea}/>
+        order:  <BubbleTeaDetail bubbleTea = {this.state.selectedBubbleTea} onSellingTea={this.handleSellTea}  onRestockingTea={this.handleRestockTea} onDeletingTea={this.handleDeleteTea}/>
       }
     } else if (this.state.formVisibleOnPage) {
       return {
--- src/components/BubbleTeaDetail.jsx
+++ src/components/BubbleTeaDetail.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import PropTypes from "prop-types"
 
 function BubbleTeaDetail(props){
-  const { bubbleTea, onRestockingTea, onSellingTea } =props;
+  const { bubbleTea, onRestockingTea, onSellingTea, onDeletingTea } =props;
 
   // if ({bubbleTea.quantity} < 1) {
   //   return {bubbleTea.quantity} == "Out of Stock";
@@ -14,14 +14,11 @@ function BubbleTeaDetail(props){
       <h3>{bubbleTea.name}</h3>
       <h3>{bubbleTea.id}</h3>
       <h4>{bubbleTea.ingredient}</h4>
-      <h4>{bubbleTea.quantity}{bubbleTea.message}</h4>
-      {/* <h4>{bubbleTea.quantity} > 0 ? {bubbleTea.quantity} : "out of stock"</h4> */}
-   
-    
-      {/* <h4>{bubbleTea.quantity < 1} ? {bubbleTea.quantity='Out of Stock'} : {bubbleTea.quantity}</h4> */}
+      <h4>{bubbleTea.quantity}</h4>
+      <h4>{bubbleTea.message}</h4>
       <button onClick = {() => props.onSellingTea(props.id)}>Sell</button>
       <button onClick = {() => props.onRestockingTea(props.id)}>Restock</button>
-      <button onClick = {() => props.onDeletingTea(props.id)}>Delete</button>
+      <button onClick = {() => props.onDeletingTea(bubbleTea.id)}>Delete</button>
       <hr/>
     </React.Fragment>
   );
